4514f7e5a789ed405e45389f707513f8d2a783f2
Carlos Alberto Alegre <ca_alegre3244@ymail.com>
2019-11-07 19:44:22 -0300

    add sweetalert2 library

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,27 @@ function init(){
     const file = e.target.files[0];
     document.getElementById('image_preview').src = URL.createObjectURL(file);
   }, false);
+
+
+ document.addEventListener("click", function (e) {
+   elem = e.target
+   if (elem.getAttribute("name") == "btn_delete") {
+      e.preventDefault()
+     Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.value) {
+        elem.parentNode.submit()
+      }
+    })
+   }
+  }, false);
 }
 
 
